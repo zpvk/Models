9e5eb71757e33cebeefc02366445438306d5489b
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-18 02:26:34 +0530

    c4

--- Tensorflow Speech Recognizer/demo.py
+++ Tensorflow Speech Recognizer/demo.py
@@ -1,4 +1,17 @@
 import tflearn
 import speech_data
 
+learning_rate = 0.0001
+traning_iters = 300000
+
+batch = word_batch = speech_data.mfcc_batch_generator(64)
+
+X,Y = next(batch)
+trainX, traningY = x,Y
+textX, textY, X,Y
+
+net = ftlearn.input_data([None, 20, 80])
+net = ftlearn.lsm(net, 128, dropout=0.8)
+net = ftlearn.fully_connrcted()
+
 
