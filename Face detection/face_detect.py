de0ddcd892c923ef6c14f462a7693bd67cf411e5
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-18 01:10:58 +0530

    jjj

--- Face detection/face_detect.py
+++ Face detection/face_detect.py
@@ -1,25 +1,27 @@
 import cv2
 
+cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 cap = cv2.VideoCapture(0)
-cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 while True:
+    # Capture frame-by-frame
     ret, frame = cap.read()
-
-    frame = cv2.cvtColor(frame,0)
-
-    detections = cascade_classifier.detectMultiScale(gray, 1.3, 5)
-
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, 0)
+    detections = cascade_classifier.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=5)
     if(len(detections) > 0):
         (x,y,w,h) = detections[0]
-        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(225,0,0),2)
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
+    # for (x,y,w,h) in detections:
+    # 	frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+    # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
-#when everything done, relase the capture
+# When everything done, release the capture
 cap.release()
+cv2.destroyAllWindows()
 
