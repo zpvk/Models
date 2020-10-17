bc2ae54a079fba8767a7e4593f291685d318d38d
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-18 00:45:22 +0530

    9

--- Face detection/face_detect.py
+++ Face detection/face_detect.py
@@ -6,6 +6,16 @@ cascade_classifier = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface
 while True:
     ret, frame = cap.read()
 
-    cv2.imshow('frame',frame)
+    frame = cv2.cvtColor(frame,0)
 
+    detections = cascade_classifier.detectMultiScale(frame)
+
+    if(len(detections)>0):
+        (x,y,w,h) = detections[0]
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(225,0,0),2)
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
     cv2.waitKey(1)
