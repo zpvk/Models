e8df2337ffb67aa5378525930ceb689feaaf78d9
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-02-02 22:23:04 +0530

    Create main.py

---
+++ pactrical tutorial ml/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+# print(data.head(10))
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1))
+Y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test, y_test)
+print(accuracy)
+
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):\
+    print(predictions[x], x_test[x], y_test[x])
